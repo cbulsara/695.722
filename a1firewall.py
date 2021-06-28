2655d55d6c21141f3f372c86b1ce91f449e2bbef
cbulsara <bulsaracyrus@gmail.com>
2021-06-28 13:45:53 -0700

    fixed firewall

--- a1firewall.py
+++ a1firewall.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 from scapy.all import *
 import argparse
+import time
 
 parser=argparse.ArgumentParser(description='Pass args to Assignment 1')
 parser.add_argument('sourceIP', metavar="sourceIP", type=str, help="Source IP")
@@ -8,34 +9,43 @@ parser.add_argument('destinationIP', metavar="destinationIP", type=str, help="De
 parser.add_argument('--dport', metavar="destinationPort", type=int, default=4443, help="Destination port (default 4443")
 
 isn = []
-delta = []
+deltaISN = []
 wait = 0
 
 def main():
     args = parser.parse_args()
     print( args )
 
-    pkts = sniff(filter="src " + args.sourceIP + \
+    pkts = sniff( filter="src " + args.sourceIP + \
                 " && dst " + args.destinationIP + \
                 " && tcp dst port " + str(args.dport), \
-                prn=process_packet)
+                prn=process_packet )
     
 
 def process_packet(packet):
-   
+    
+    global isn
+    global deltaISN
+    global wait
+
+    print(f"{len(isn)}")
+    print(f"{len(deltaISN)}")
+    
     isn.append(packet[TCP].seq)
 
     if len(isn) > 1:
-        delta.append(abs(isn[-1] - isn [-2]))
+        print(f"Debug: appending {abs(isn[-1] - isn[-2])} to delta." )
+        deltaISN.append(abs(isn[-1] - isn [-2]))
     
-    if len(delta) > 5:
-        delta = delta[1:]
+    if len(deltaISN) > 5:
+        print("Debug: trimming delta to 5")
+        deltaISN = deltaISN[1:]
     
-    if sum(delta) / len(delta) > 5:
-        print("Unusual gap between ISNs detected. Throttling connection.")
-        wait = 5
+        if sum(deltaISN) / len(deltaISN) > 100:
+            print("Unusual gap between ISNs detected. Throttling connection.")
+            wait = 5
     
-    sleep(wait)
+    time.sleep(wait)
 
     sendp(packet)
 
