fe009a853e1bf1945b6383f8925d45e7f68768d4
root <bulsarac@gmail.com>
2021-07-18 14:55:14 -0700

    updated server.py

--- server.py
+++ server.py
@@ -1,18 +1,22 @@
-
 #!/usr/bin/env python3
 
 import http.server as SimpleHTTPServer
 import socketserver as SocketServer
 import logging
 
-PORT = 8000
+PORT = 80
 
 class GetHandler(
         SimpleHTTPServer.SimpleHTTPRequestHandler
         ):
 
     def do_GET(self):
-        logging.error(self.headers)
+        #logging.error(self.headers)
+        if 'Content-Md5' in self.headers:
+            print(self.headers['Content-MD5'])
+            print(chr(int(self.headers['Content-MD5'])))
+
+
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
 
